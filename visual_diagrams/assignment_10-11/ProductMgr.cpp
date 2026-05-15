64db7bd4c995da0cd95d955ac0b08f15fcbdd2d8
Andrey <76159505+sokoluk-andrey@users.noreply.github.com>
2026-05-15 16:08:00 +0300

    Create ProductMgr.cpp

---
+++ visual_diagrams/assignment_10-11/ProductMgr.cpp
@@ -0,0 +1,16 @@
+#include "ProductMgr.h"
+#include "Product.h"
+
+ProductMgr::ProductMgr()
+{
+}
+
+ProductMgr::~ProductMgr()
+{
+}
+
+Product* ProductMgr::GetProduct(long ItemNo)
+{
+    // TODO: query ProductCollection and return the matching product
+    return nullptr;
+}
