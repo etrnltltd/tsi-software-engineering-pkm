15c07f6896f6e8412eae74a50f448bde57721afa
Andrey <76159505+sokoluk-andrey@users.noreply.github.com>
2026-05-15 16:08:19 +0300

    Create Product.cpp

---
+++ visual_diagrams/assignment_10-11/Product.cpp
@@ -0,0 +1,16 @@
+#include "Product.h"
+
+Product::Product()
+    : ProductID(0), ProductUnitPrice(0.0)
+{
+}
+
+Product::~Product()
+{
+}
+
+Product* Product::GetProduct(long ItemNo)
+{
+    // TODO: implement product retrieval
+    return nullptr;
+}
