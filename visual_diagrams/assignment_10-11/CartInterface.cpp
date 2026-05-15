4d9a54ac3ff05ea5f82e168157465b68629b850c
Andrey <76159505+sokoluk-andrey@users.noreply.github.com>
2026-05-15 16:07:15 +0300

    Create CartInterface.cpp

---
+++ visual_diagrams/assignment_10-11/CartInterface.cpp
@@ -0,0 +1,15 @@
+#include "CartInterface.h"
+
+CartInterface::CartInterface()
+{
+}
+
+CartInterface::~CartInterface()
+{
+}
+
+bool CartInterface::AddItem(long ItemNo)
+{
+    // TODO: implement boundary logic — forward request to CartMgr
+    return false;
+}
