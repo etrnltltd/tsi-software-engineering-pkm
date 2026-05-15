265f014994f23c8ef84ff69a430ac3cc8bb038a1
Andrey <76159505+sokoluk-andrey@users.noreply.github.com>
2026-05-15 16:08:49 +0300

    Create CartCollection.cpp

---
+++ visual_diagrams/assignment_10-11/CartCollection.cpp
@@ -0,0 +1,16 @@
+#include "CartCollection.h"
+#include "Product.h"
+
+CartCollection::CartCollection()
+{
+}
+
+CartCollection::~CartCollection()
+{
+}
+
+bool CartCollection::AddItem(Product* NewItem)
+{
+    // TODO: append the product to the aggregated CartItems
+    return false;
+}
