b371801fb423de697dd680e487bf80b87d844644
Andrey <76159505+sokoluk-andrey@users.noreply.github.com>
2026-05-15 16:08:35 +0300

    Create ProductCollection.cpp

---
+++ visual_diagrams/assignment_10-11/ProductCollection.cpp
@@ -0,0 +1,16 @@
+#include "ProductCollection.h"
+#include "Product.h"
+
+ProductCollection::ProductCollection()
+{
+}
+
+ProductCollection::~ProductCollection()
+{
+}
+
+Product* ProductCollection::FindProduct(long ItemNo)
+{
+    // TODO: search the aggregated Products and return the match
+    return nullptr;
+}
