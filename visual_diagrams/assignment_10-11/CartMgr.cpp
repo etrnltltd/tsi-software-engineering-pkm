8fb5d6a26666540ccfabf9483cc3f99972dc06bb
Andrey <76159505+sokoluk-andrey@users.noreply.github.com>
2026-05-15 16:07:40 +0300

    Create CartMgr.cpp

---
+++ visual_diagrams/assignment_10-11/CartMgr.cpp
@@ -0,0 +1,15 @@
+#include "CartMgr.h"
+
+CartMgr::CartMgr()
+{
+}
+
+CartMgr::~CartMgr()
+{
+}
+
+bool CartMgr::AddItem(long ItemNo)
+{
+    // TODO: coordinate with ProductMgr and CartCollection
+    return false;
+}
